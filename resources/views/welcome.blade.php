ac12c146bd529672a07ecf460a2d10d318c45271
juanlugrove <grovejuanluis@gmail.com>
2022-02-22 13:50:43 +0100

    Imagen y diseño inicial index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,31 +11,32 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="">
         <style>
             body {
                 font-family: 'Nunito', sans-serif;
+                background-color: rgb(44, 43, 43)
             }
         </style>
     </head>
-    <body class="antialiased">
-        <div class="row">
-            <div class="">
-                <div class="bg-danger" style="width:200px; height: 200px;"></div>
+    <body>
+        <div class="container-xl">
+            <div class="row mt-3">
+                <div class="col">
+                    <img src="img/logo.png" class="mx-auto d-block" style="width: 400px">
+                </div>
             </div>
             @if (Route::has('login'))
                 <div class="col text-center">
                     @auth
                         <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                        <a href="{{ route('login') }}" class="btn btn-warning btn-lg">Log in</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                            <a href="{{ route('register') }}" class="btn btn-warning btn-lg">Register</a>
                         @endif
                     @endauth
                 </div>
-                <div></div>
             @endif
         </div>
     </body>
