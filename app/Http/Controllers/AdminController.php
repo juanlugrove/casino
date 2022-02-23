857cff9aba41c99927845f831787b35b263e20cb
JosedaaGR <josedaagonzalz11@gmail.com>
2022-02-23 09:40:00 +0100

    Registros de admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,11 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index(){
-        return view('admin.index');
+
+        $users= User::where('role','=','user')->get();
+        return view('admin.index',compact('users'));
     }
 }
