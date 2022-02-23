2d6e9ecc6df7f51a73731ae2a8952fd263785fc6
juanlugrove <grovejuanluis@gmail.com>
2022-02-23 08:39:28 +0100

    Arreglado problema de admin Inicio de sesion

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
@@ -18,9 +19,19 @@ class UserSeeder extends Seeder
         $admin=new User();
         $admin->name="JoseDaniel";
         $admin->email="admin@admin.com";
-        $admin->password="adminadmin";
+        $admin->password="$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";
         $admin->role="admin";
+        $admin->email_verified_at= now();
+        $admin->remember_token = Str::random(10);
         $admin->save();
+        $admin2=new User();
+        $admin2->name="JuanLuis";
+        $admin2->email="admin2@admin.com";
+        $admin2->password="$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";
+        $admin2->role="admin";
+        $admin2->email_verified_at= now();
+        $admin2->remember_token = Str::random(10);
+        $admin2->save();
         User::factory(25)->create();
     }
 }
