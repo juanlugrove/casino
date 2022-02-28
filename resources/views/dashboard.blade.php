e06b9873191b3a74704a576be36bf7ada4ef058b
juanlugrove <grovejuanluis@gmail.com>
2022-02-28 13:32:53 +0100

    Agregadas imagenes juegos

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,16 +1,28 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
+            {{ __('Inicio') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
-                </div>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 d-flex justify-content-center">
+            {{-- <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg"></div> --}}
+            <div class="card bg-white overflow-hidden shadow-sm sm:rounded-lg" style="width: 18rem;">
+                <a href="" style="cursor: not-allowed;">
+                    <img src="img/dados.jpg" class="card-img-top" alt="...">
+                </a>
+                    <div class="card-body p-2">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+            </div>
+            <div class="card bg-white overflow-hidden shadow-sm sm:rounded-lg" style="width: 18rem;">
+                <a href="" style="cursor: not-allowed;">
+                    <img src="img/proximamente.jpg" class="card-img-top" alt="...">
+                </a>
+                    <div class="card-body p-2">
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
             </div>
         </div>
     </div>
