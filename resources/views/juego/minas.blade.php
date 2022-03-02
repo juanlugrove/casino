39f017ce42212a2d8eb16bfbab45d7157ecc2328
JosedaaGR <josedaagonzalz11@gmail.com>
2022-03-02 08:27:35 +0100

    Cambio de nombre Juego

--- resources/views/juego/minas.blade.php
+++ resources/views/juego/minas.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight" style="color:white;">
-            {{ __('Dados') }}
+            {{ __('Minas') }}
         </h2>
     </x-slot>
 
@@ -61,9 +61,9 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form action="" method="POST">
-                        @csrf
-                        @method("put")
+                    <form action="" method="GET">
+                        {{-- @csrf
+                        @method("put") --}}
                     <div class="contenedor">
                         {{-- minas --}}
                         <table>
